2774931393e1762aa1426e1b536fda11e9025623
Niu, Lan <qxxnnn@gmail.com>
2019-09-26 14:45:47 +1200

    second upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<?php
+ini_set("error_reporting",E_ALL);
+ini_set("log_errors","1");
+ini_set("error_log","php_errors.txt");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{background-color: lightblue;}
+.pictures {
+display: inline-grid;
+grid-template-columns: auto auto auto auto auto;
+padding: 40px;
+grid-area: pic;
+margin-right:20%;
+}
+
+.grid-item {
+
+background-color: rgba(255, 255, 255, 0.8);
+border: 5px solid rgba(0, 0, 0, 0.8);
+padding: 5px;
+
+}
+.category{text-align: center}
+img{
+width: 100%;
+background-color: white;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+div.container {
+text-align: center;
+
+}
+.but{position:fixed; 
+
+border: none;
+color: white;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 2px 2px;
+cursor: pointer;
+top: 20%;
+right: 10%;
+}
+.buttonRefresh{padding:20px;border-radius: 60%;}
+</style>
+<title>Mars View</title>
+</head>
+<body>
+<script>
+function GotoFavourite() {
+  location.replace("https://webapp.cms.waikato.ac.nz/~ln46/Favourite.php");
+}
+function GotoStatistics() {
+  location.replace("https://webapp.cms.waikato.ac.nz/~ln46/Statistics.php");
+}
+</script>
+<div style="text-align:center; font-size:25px;font-family:Comic Sans MS, cursive, sans-serif;border-bottom: 2px solid black;">
+<h1>
+Mars View
+</h1>
+</div>
+<div class="grid-container">
+<div class="pictures">
+
+
+<?php
+doit();
+function doit(){
+$x = 0;
+$conn = new mysqli("mysql.cms.waikato.ac.nz", "zz169", "my11154850sql","zz169");
+if ($conn==FALSE) {
+die("<p>ERROR: Unable to connect: " . $conn->connect_error);
+}
+
+$picArray=array();
+$n=0;
+while($n<25)
+{
+$number=rand(0,10432);
+
+if(check($picArray,$number)==true)
+{
+array_push($picArray,$number);
+$n++;
+}
+
+}
+while($x < 25) {
+
+	
+$sql = "SELECT fileName FROM ass3 WHERE ID=$picArray[$x]";
+
+	$result = $conn->query($sql);
+
+    while($row = $result->fetch_assoc()) {
+        $name=$row["fileName"];
+    }
+
+    echo "<div class='grid-item'><div><img id=$x src='https://www.cms.waikato.ac.nz/~mmayo/compx222/mars/images/$name' alt='test!!!'></div> <div class='container'>
+  <p>****</p>
+  </div></div>";
+    $x++;
+}
+$conn->close();
+
+}
+function check($ar,$number) {
+$arrlength = count($ar);
+for($x = 0; $x < $arrlength; $x++) {
+if($ar[$x]==$number)
+{return false;}
+}
+return true;
+}
+?>
+
+
+</div>
+<div class="but">
+<button class="buttonRefresh" onclick=location.reload() >Refresh</button>
+<br>
+<br>
+<br>
+<button class="buttonRefresh" onclick= 	GotoFavourite() >Favourite</button>
+<br>
+<br>
+<br>
+<button class="buttonRefresh" onclick= GotoStatistics() >Statistics</button>
+
+
+</div>  
+</div>
+</body>
+</html>
+
